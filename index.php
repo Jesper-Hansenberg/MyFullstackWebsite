4181fd6686e94999223bff924ff8e008727e2a6c
Jesper Madsen <jema@hansenberg.dk>
2022-09-07 20:11:28 +0200

    removed sql injection example + md5 weakness
    removed sql injection example + md5 weakness

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: 2_Lesson/BadLogin.php");
+    header("location: 2_Lesson/login.php");
     exit;
 }
 ?>
